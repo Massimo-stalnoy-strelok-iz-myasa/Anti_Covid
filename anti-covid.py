776458c72c9ba0bef13d29239f802483d1d908e2
Massimo-stalnoy-strelok-iz-myasa <57673259+Massimo-stalnoy-strelok-iz-myasa@users.noreply.github.com>
2020-11-26 19:48:20 +0300

    Add files via upload

--- anti-covid.py
+++ anti-covid.py
@@ -1,38 +1,106 @@
 import telebot
-from PIL import Image
 from telebot import types
 import urls
+import COVID19Py
+
+
+flag = True
+
+covid19 = COVID19Py.COVID19()
 bot = telebot.TeleBot('1423490246:AAH86ALx0alw1yQmjVcAxmTqqlR95V0kUC8')
 urls.get_content(urls.html.text)
 urls.get_content_1(urls.html_1.text)
+urls.get_content_2(urls.html_2.text)
+# Функция, что сработает при отправке команды Старт
+# Здесь мы создаем быстрые кнопки, а также сообщение с привествием
+
+
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.send_message(
-        message.chat.id,
-        '''Приветствую''', reply_markup=keyboard())
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn1 = types.KeyboardButton('Во всём мире')
+    btn2 = types.KeyboardButton('Украина')
+    btn3 = types.KeyboardButton('Россия')
+    btn4 = types.KeyboardButton('Беларусь')
+    btn5 = types.KeyboardButton('Италия')
+    btn6 = types.KeyboardButton('Германия')
+    btn7 = types.KeyboardButton('Япония')
+    btn8 = types.KeyboardButton('Франция')
+    markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8)
+    flag = True
+    send_message = f"<b>Привет {message.from_user.first_name}!</b>\nЧтобы узнать данные про коронавируса напишите " \
+        f"название страны, например: США, Украина, Россия и так далее, если хотите просмотреть локальные данные введите " \
+        f"команду /local, если хотите вернуться к общей статистике введите /all"
+    bot.send_message(message.chat.id, send_message, parse_mode='html', reply_markup=markup)
 
 
-@bot.message_handler(content_types=["text"])
-def send_anytext(message):
-    chat_id = message.chat.id
-    if message.text.lower() == 'индекс самоизоляции':
-        text = 'Введите ваш город'
-        bot.send_message(chat_id, text, reply_markup=keyboard())
-    if message.text.lower() in urls.all_cities:
-        ind = urls.all_cities.index(message.text.lower())
-        text = 'Город найден: Индекс самоизоляции в вашем городе ' + str(urls.all_index[ind])
-        bot.send_message(chat_id, text, reply_markup=keyboard())
 
 
+# Функция, что сработает при отправке какого-либо текста боту
+# Здесь мы создаем отслеживания данных и вывод статистики по определенной стране
+@bot.message_handler(content_types=['text'])
+def mess(message):
+    global flag
+    final_message = ""
+    get_message_bot = message.text.strip().lower()
+
+    if get_message_bot == "/local":
+        send_message = f"Введите ваш город, чтобы узнать индекс самоизоляции или индекс активности граждан. " \
+                       f"Введите ваш адресс, чтобы узнать есть ли заболевшие в вашем доме"
+        bot.send_message(message.chat.id, send_message)
+        flag = False
+
+    elif get_message_bot == "/all":
+        send_message = f"Выберите страну из списка предложенных "
+        bot.send_message(message.chat.id, send_message)
+        final_message = "1"
+        flag = True
+    elif get_message_bot == "сша":
+        location = covid19.getLocationByCountryCode("US")
+    elif get_message_bot == "украина":
+        location = covid19.getLocationByCountryCode("UA")
+    elif get_message_bot == "россия":
+        location = covid19.getLocationByCountryCode("RU")
+    elif get_message_bot == "беларусь":
+        location = covid19.getLocationByCountryCode("BY")
+    elif get_message_bot == "италия":
+        location = covid19.getLocationByCountryCode("IT")
+    elif get_message_bot == "франция":
+        location = covid19.getLocationByCountryCode("FR")
+    elif get_message_bot == "германия":
+        location = covid19.getLocationByCountryCode("DE")
+    elif get_message_bot == "япония":
+        location = covid19.getLocationByCountryCode("JP")
+    elif get_message_bot == "во всём мире":
+        location = covid19.getLatest()
+        final_message = f"<u>Данные по всему миру:</u>\n<b>Заболевших: </b>{location['confirmed']:,}\n<b>Сметрей: </b>{location['deaths']:,}"
+        bot.send_message(message.chat.id, final_message, parse_mode='html')
+
+    if final_message == "" and flag:
+        date = location[0]['last_updated'].split("T")
+        time = date[1].split(".")
+        final_message = f"<u>Данные по стране:</u>\nНаселение: {location[0]['country_population']:,}\n" \
+                f"Последнее обновление: {date[0]} {time[0]}\nПоследние данные:\n<b>" \
+                f"Заболевших: </b>{location[0]['latest']['confirmed']:,}\n<b>Сметрей: </b>" \
+                f"{location[0]['latest']['deaths']:,}"
+        bot.send_message(message.chat.id, final_message, parse_mode='html')
+    else:
+        res = [i for i in urls.all_streets if get_message_bot in i]
+        res = str(res)
+        if get_message_bot in urls.all_activity_cities:
+            ind = urls.all_activity_cities.index(message.text.lower())
+            final_message = 'Город найден: Индекс активности в вашем городе ' + str(urls.all_activity[ind])
+            bot.send_message(message.chat.id, final_message)
+        if get_message_bot in urls.all_cities:
+            ind = urls.all_cities.index(message.text.lower())
+            final_message = 'Город найден: Индекс самоизоляции в вашем городе ' + str(urls.all_index[ind])
+            bot.send_message(message.chat.id, final_message)
+
+        elif get_message_bot in res:
+            final_message = 'Ваш дом найден в базе данных, рекомендуем надевать перчатки и маску при выходе из дома'
+            bot.send_message(message.chat.id, final_message)
 
 
-def keyboard():
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-    btn1 = types.KeyboardButton('Индекс Самоизоляции')
-    markup.add(btn1)
-    return markup
-
-
-if __name__ == "__main__":
-    bot.polling(none_stop=True)
+# Это нужно чтобы бот работал всё время
+bot.polling(none_stop=True)
--- urls.py
+++ urls.py
@@ -1,12 +1,22 @@
 import requests
 from bs4 import BeautifulSoup as BS
 
+
+
 URL = 'https://covid.osnova.news/self-isolation/'
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
            'accept': '*/*'}
 
 URL_1 = 'https://coronavirus-online.moscow/sluchai-koronavirusa-v-moskve/'
 HEADERS_1 = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36 Edg/87.0.664.41',
+           'accept': '*/*'}
+
+URL_2 = 'https://covid.osnova.news/activitys/'
+HEADERS_2 = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36 Edg/87.0.664.47',
+           'accept': '*/*'}
+
+URL_3 = 'https://coronavirusstat.ru/country/moskva/'
+HEADERS_3 = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36 Edg/87.0.664.47',
            'accept': '*/*'}
 
 
@@ -22,6 +32,7 @@ def get_content(html):
         all_cities.append((' '.join(name.get_text().lower().split())).split()[0])
         all_index.append((' '.join(name.get_text().lower().split())).split()[1])
 
+
 def get_content_1(html):
     soup = BS(html, 'html.parser')
     otvet = ""
@@ -30,16 +41,40 @@ def get_content_1(html):
     dates = soup.find_all('td', class_='column-3')
     for i in range(len(streets)):
         otvet = streets[i].get_text() + " " + houses[i].get_text() + " " + dates[i].get_text()
-        all_streets.append(otvet)
+        all_streets.append(otvet.lower())
         otvet = ""
 
 
+def get_content_2(html):
+    soup = BS(html, 'html.parser')
+    activities = soup.find_all('div', class_='card-body p-2 pl-3 text-nowrap')
+    for activity in activities:
+        all_activity_cities.append(activity.get_text().lower().split()[0])
+        all_activity.append(activity.get_text().lower().split()[1])
 
+
+def get_content_3(html):
+    soup = BS(html, 'html.parser')
+    ills = soup.find_all('td')
+    for ill in ills:
+        print(ill.get_text().split())
+
+
+all_ill = []
 all_streets = []
 all_cities = []
 all_index = []
+all_activity = []
+all_activity_cities = []
 
 html = get_html(URL)
 html_1 = get_html(URL_1)
+html_2 = get_html(URL_2)
+html_3 = get_html(URL_3)
+
 get_content(html.text)
 get_content_1(html_1.text)
+get_content_2(html_2.text)
+get_content_3(html_3.text)
+
+
